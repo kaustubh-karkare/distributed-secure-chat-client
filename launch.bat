38c2fe0b04cd4d9bc1ae942cadaa754e59d4402f
Kaustubh Karkare <kaustubh.karkare@gmail.com>
2012-09-26 00:24:44 +0530

    Upload

---
+++ launch.bat
@@ -0,0 +1,8 @@
+
+javac ChatNetwork.java
+start java -cp .;sqlitejdbc-v056.jar ChatNetwork root root
+start java -cp .;sqlitejdbc-v056.jar ChatNetwork anurag dubey
+start java -cp .;sqlitejdbc-v056.jar ChatNetwork dhruva kumar
+start java -cp .;sqlitejdbc-v056.jar ChatNetwork kaustubh karkare
+start java -cp .;sqlitejdbc-v056.jar ChatNetwork kirtivardhan rathore
+start java -cp .;sqlitejdbc-v056.jar ChatNetwork vishnu mohandas
